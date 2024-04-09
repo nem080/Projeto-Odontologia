7f7e2e129e7bc89f1583fd79befbc46ab07bd302
João <nem878787@gmail.com>
2024-04-09 01:39:13 -0300

    terminar amanha retomar da estilização, parte da msg concluida

--- js/module/formContato.js
+++ js/module/formContato.js
@@ -1,48 +1,64 @@
 export function initFormContato() {
-    // Função para validar o formulário
-    
+    const corpoForm = document.querySelector('#form-contato');
+    const btnEnviar = document.querySelector('#btn-from-contato'); // Selecionar o elemento do botão, não o valor
+    const msgEnvio = document.querySelector('.resposta-from')
+    console.log(msgEnvio);
+
+    function addLoading() {
+        btnEnviar.innerHTML = '<img src="../img/loadAzul.png">';
+    }
+
+    function confirEnvio() {
+        msgEnvio.innerHTML = 'Dados Enviado Com Sucesso, Logo Retornaremos'
+        msgEnvio.style.display = 'block'
+        setTimeout(function () {
+            msgEnvio.remove();
+        }, 7000);
+    }
+
+
+    function removeLoading() {
+        btnEnviar.innerHTML = 'Enviar';
+    }
+
+    function handleSubmit(event) {
+        event.preventDefault();
+        addLoading()
+        const name = document.querySelector('input[name="nome"]').value;
+        const email = document.querySelector('input[name="email"]').value;
+        const telefone = document.querySelector('input[name="telefone"]').value;
+        const msg = document.querySelector('textarea[name="mensagem"]').value;
+
+        fetch('https://api.sheetmonkey.io/form/dgvF4tuQp9hY8aqiujk4hi', {
+            method: 'POST',
+            headers: {
+                'accept': 'application/json',
+                'content-type': 'application/json',
+            },
+            body: JSON.stringify({
+                NOME: name,
+                EMAIL: email,
+                MENSAGEM: msg,
+                TELEFONE: telefone
+            })
+        }).then(() => {
+            confirEnvio();
+            removeLoading();
+            // Limpar os campos após o envio
+            document.querySelector('input[name="nome"]').value = '';
+            document.querySelector('input[name="email"]').value = '';
+            document.querySelector('input[name="telefone"]').value = '';
+            document.querySelector('textarea[name="mensagem"]').value = '';
+
+            setTimeout(function () {
+;                location.reload();
+            }, 3000);
+        });
+        console.log(name, email, telefone, msg);
+
+
+    }
+
+    corpoForm.addEventListener('submit', handleSubmit);
+
 }
-
-const corpoForm = document.querySelector('#form-contato');
-const btnEnviar = document.querySelector('#btn-from-contato'); // Selecionar o elemento do botão, não o valor
-// console.log(btnEnviar);
-
-function addLoading() {
-    btnEnviar.innerHTML = '<img src="../img/loadAzul.png">';
-}
-function removeLoading() {
-    btnEnviar.innerHTML = 'Enviar';
-}
-
-function handleSubmit(event) {
-    event.preventDefault();
-    addLoading()
-    const name = document.querySelector('input[name="nome"]').value;
-    const email = document.querySelector('input[name="email"]').value;
-    const telefone = document.querySelector('input[name="telefone"]').value;
-    const msg = document.querySelector('textarea[name="mensagem"]').value;
-
-    fetch('https://api.sheetmonkey.io/form/dgvF4tuQp9hY8aqiujk4hi', {
-        method: 'POST',
-        headers: {
-            'accept': 'application/json',
-            'content-type': 'application/json',
-        },
-        body: JSON.stringify({
-            NOME: name,
-            EMAIL: email,
-            MENSAGEM: msg,
-            TELEFONE: telefone
-        })
-    }).then(() => {
-        removeLoading();
-        // Limpar os campos após o envio
-        document.querySelector('input[name="nome"]').value = '';
-        document.querySelector('input[name="email"]').value = '';
-        document.querySelector('input[name="telefone"]').value = '';
-        document.querySelector('textarea[name="mensagem"]').value = '';
-    });
-    console.log(name, email, telefone, msg);
-}
-
-corpoForm.addEventListener('submit', handleSubmit);
